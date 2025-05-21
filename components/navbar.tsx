889bedabfafbc153ed919472af3a2243d0087942
armstrng-series <enefearmstrong@gmail.com>
2025-05-21 17:27:23 +0100

    updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,13 +16,13 @@ export default function Navbar() {
   return (
     <nav className="bg-white border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
+        <div className="flex justify-between ">
           <div className="flex items-center">
             <Link href="/" className="flex-shrink-0 flex items-center">
               {/* <span className="text-xl font-bold text-dark">CorpVision</span> */}
               <Image
-                                    width={100}
-                                    height={100} src="./img/logo.jpeg" className="w-52 h-14 rounded-xl" alt="logo image" />
+                  width={80}
+                  height={80} src="./img/logo_2.png" className="w-55 h-14 rounded-xl" alt="logo image" />
             </Link>
           </div>
           <div className="hidden md:flex items-center space-x-8">
