34da332e0abd647369b275acc1a59b25bf6ad927
armstrng-series <enefearmstrong@gmail.com>
2025-05-25 21:30:51 +0100

    edited config file

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -58,21 +58,11 @@ export default function Navbar() {
       {isMenuOpen && (
         <div className="md:hidden bg-white border-b border-gray-200">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <Link href="/#about" className="block px-3 py-2 text-dark hover:text-secondary" onClick={toggleMenu}>
-              About
-            </Link>
-            <Link href="/services" className="block px-3 py-2 text-dark hover:text-secondary" onClick={toggleMenu}>
-              Services
-            </Link>
-            <Link href="/#team" className="block px-3 py-2 text-dark hover:text-secondary" onClick={toggleMenu}>
-              Team
-            </Link>
-            <Link href="/#testimonials" className="block px-3 py-2 text-dark hover:text-secondary" onClick={toggleMenu}>
-              Testimonials
-            </Link>
-            <Link href="/blog" className="block px-3 py-2 text-dark hover:text-secondary" onClick={toggleMenu}>
-              Blog
-            </Link>
+          {navigation.map((nav, index) => (
+                <Link key={`mobile_nav_${index}`} href={`${nav.url}`} className="text-dark hover:text-secondary">
+                  {nav?.name}
+              </Link>
+            ))}
             <Link href="/contact" className="block px-3 py-2" onClick={toggleMenu}>
               <Button className="w-full bg-secondary hover:bg-secondary/90 text-white">Contact Us</Button>
             </Link>
